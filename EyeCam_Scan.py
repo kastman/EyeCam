8912e89f2730692d061e160dd7247769c4ebcb80
Erik Kastman <erik.kastman@gmail.com>
2017-04-04 09:51:54 -0400

    Add 3 seconds to adjust for extra scan time

--- EyeCam_Scan.py
+++ EyeCam_Scan.py
@@ -124,14 +124,14 @@ except ValueError:
 # Set Run Number and Duration based on Age and Scan Type
 if expInfo['scan type'] == 'mbPCASL':
     nRuns = 1
-    runDuration = 322.2  # sec
+    runDuration = 325.2  # sec
 else:  # expInfo['scan type'] == 'REST'
     if age >= 8:
         nRuns = 2
-        runDuration = 390.4  # sec plus TR adjustmnet
+        runDuration = 393.4  # sec plus TR adjustmnet
     else:  #if 5-7yo
         nRuns = 3
-        runDuration = 180
+        runDuration = 183
 
 # Set HCP Style Params
 triggerKey = config['trigger']  #5 at Harvard
